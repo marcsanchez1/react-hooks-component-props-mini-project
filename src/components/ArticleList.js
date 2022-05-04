1fe0b655e0392004f773135e279241a90b3d7c72
Marc <marcsanchez@me.com>
2022-05-04 10:25:24 -0400

    Submitting assignment

---
+++ src/components/ArticleList.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import Article from './Article'
+
+function ArticleList({posts}) {
+
+  const articles = posts.map((post) => {
+    return  (
+      <Article key={post.id}
+        title={post.title}
+        data={post.date}
+        preview={post.preview}
+        minutes={post.minutes}
+      />
+    )
+  })
+
+  return (
+    <main>
+      {articles}
+    </main>
+  )
+}
+
+export default ArticleList
